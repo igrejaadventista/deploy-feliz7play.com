7cb420232c389e33785338e570e2e2eb5d42341e
Eli Mendonca <contato@elimendonca.com>
2021-05-28 11:24:44 -0300

    Ajuste nas variaveis de ambiente

--- app/_index.php
+++ app/_index.php
@@ -6,6 +6,7 @@ echo $_ENV['WP_DB_PASSWORD'] ."\n";
 echo $_ENV['WP_DB_USER'] ."\n";
 echo $_ENV['WP_S3_ACCESS_KEY'] ."\n";
 echo $_ENV['WP_S3_SECRET_KEY'] ."\n";
+echo $_ENV['WP_S3_BUCKET'] ."\n";
 
 phpinfo(INFO_ENVIRONMENT);
 phpinfo(INFO_VARIABLES);
