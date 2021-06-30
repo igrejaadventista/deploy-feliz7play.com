9a736e882f953a07cafdfd805eb9d50e43ccc73a
Tiago Papile <tpapile@studiovisual.com.br>
2021-06-30 11:03:18 -0300

    Cria footer

--- app/wp-content/themes/feliz7play/footer.php
+++ app/wp-content/themes/feliz7play/footer.php
@@ -1,3 +1,86 @@
+        <div style="height: 60vh;"></div>
+        <footer class="footer">
+            <div class="main">
+                <div class="container-fluid">
+                    <div class="left">
+                        <img src="https://files.adventistas.org/feliz7play/v2/sites/2/2021/06/logo-white.svg" alt="">
+                        
+                        <nav>
+                            <ul>
+                                <li>
+                                    <a href="https://quemsomos.com">
+                                        <span>Quem somos</span>
+                                    </a>
+                                </li>
+                                
+                                <li>
+                                    <a href="https://envieseuvideo.com">
+                                        <span>Envie seu video</span>
+                                    </a>
+                                </li>
+                                
+                                <li>
+                                    <a href="https://contato.com">
+                                        <span>Contato</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    
+                    <div class="socials">
+                        <span>Nossas redes sociais:</span>
+                        
+                        <ul>
+                            <li>
+                                <a href="https://facebook.com" target="_blank" class="f7-icon" rel="noreferrer">
+                                    <i class="icon-facebook"></i>
+                                </a>
+                            </li>
+                            
+                            <li>
+                                <a href="https://youtube.com" target="_blank" class="f7-icon" rel="noreferrer">
+                                    <i class="icon-youtube"></i>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="https://twitter.com" target="_blank" class="f7-icon" rel="noreferrer">
+                                    <i class="icon-twitter"></i>
+                                </a>
+                            </li>
+                            
+                            <li>
+                                <a href="" target="_blank" class="f7-icon" rel="noreferrer">
+                                    <i class="icon-facebook"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="copy">
+                <div class="container-fluid">
+                    <p>Copyright © 2012-2021 Igreja Adventista. Todos os direitos reservados.</p>
+                    
+                    <ul>
+                        <li>
+                            <a href="https://biblia.com.br" target="_blank" rel="noreferrer">Biblia.com.br</a>
+                        </li>
+                        
+                        <li>
+                            <a href="https://adventistas.org" target="_blank" rel="noreferrer">Adventistas.org</a>
+                        </li>
+                        
+                        <li>
+                            <a href="https://novotempo.com" target="_blank" rel="noreferrer">Novotempo.com</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+        
         <?php wp_footer(); ?>
     </body>
 </html>
