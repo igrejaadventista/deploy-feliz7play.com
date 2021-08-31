dbe180ea8d9d55b9b8da56c5490b164d31cb2493
mizael isaltino <mizael.isaltino@gmail.com>
2021-08-31 12:27:43 -0300

    -- Reordenando a resposta do end-point genre

--- app/wp-content/themes/feliz7play/classes/rotas_api/functions_rest.php
+++ app/wp-content/themes/feliz7play/classes/rotas_api/functions_rest.php
@@ -15,8 +15,12 @@
         $items = array();
         $controle = array();
 
+        
+
         $posts = get_posts($args);
         foreach ($posts as $post){
+
+
 
             $id = $post->ID;
 
@@ -98,6 +102,131 @@
         }
 
         return $items;
+    }
+
+    function get_line_post_genre($args, $limited = false){
+
+        $items = array();
+        $controle = array();
+
+        $posts = get_posts($args);
+        foreach ($posts as $post){
+
+            $id = $post->ID;
+            $meta = get_post_meta($id);
+
+            $video_type = $meta['post_video_type'][0];
+
+            
+
+            $collection = return_parent_collection(get_the_terms($id, 'collection')[0]);
+            $id_check = ($video_type == 'Single') ? $id : $collection->term_id;
+
+            if(!in_array($id_check , $controle)){
+
+                array_push($controle, $id_check);
+
+                $values = ($video_type == 'Single') ? get_post_infos($post) : get_collection_infos($collection);
+
+                array_push($items, $values);
+
+                if($limited > 0){
+                    if($limited == count($controle)){
+                        break;
+                    }
+                }
+            }
+        }
+        
+        return $items;
+    }
+
+    function return_parent_collection($collection){
+
+        if($collection->parent){
+            return get_term($collection->parent, 'collection');
+        }
+
+        return $collection;
+    }
+
+    function get_collection_infos($collection){
+
+
+        $id = $collection->term_id;
+        $meta = get_term_meta($id);
+
+        return $collection;
+
+        return array(
+            'id' => $id,
+            'title' => $collection->name,
+            'slug' => $collection->slug,
+            'video_type' =>$collection->taxonomy,
+            'video_thumbnail' => wp_get_attachment_image_src($meta['collection_image'][0])[0],
+            'video_image_hover' => false
+        );
+    }
+
+    function get_post_infos($post){
+        
+        $id = $post->ID;
+        $meta = get_post_meta($id);
+        
+        $title =                $post->post_title;
+        $slug =                 $post->post_name;
+        $video_type =           $meta['post_video_type'][0];
+        $video_episode =        $meta['video_episode'][0];
+        $subtitle =             $meta['post_subtitle'][0];
+        $description =          wp_strip_all_tags($meta['post_blurb'][0]);
+        $video_host =           $meta['post_video_host'][0];
+        $video_id =             $meta['post_video_id'][0];
+        
+        $post_download_link =   $meta['link_download_app'][0];
+        $download =             $meta['download'][0];
+        $post_year =            $meta['post_year'][0];
+        $post_video_rating =    $meta['post_video_rating'][0];
+        $redes =                get_field('redes', $id);
+        $production =           get_field('production', $id);
+        $collection =           get_the_terms($id, 'collection')[0];
+
+        if($collection){
+            $collection->parent_slug = get_term($collection->parent, 'collection')->slug;
+        }
+
+        $genre =                get_the_terms($id, 'genre')[0];
+        $video_lenght =         $meta['post_video_length'][0];
+        $video_quality =        $meta['post_video_quality'][0];
+        
+        $video_thumbnail =      wp_get_attachment_image_src($meta['video_thumbnail'][0] == "" || is_null($meta['video_thumbnail'][0]) ? $meta['video_image_hover'][0] : $meta['video_thumbnail'][0])[0];
+        $video_image_hover =    wp_get_attachment_image_src($meta['video_image_hover'][0])[0];
+
+        $link =                 get_link_site_next($slug, $video_type, $collection);
+        
+        return array(
+            'id' => $id,
+            'title' => $title, 
+            'slug' => $slug, 
+            'video_type' => $video_type, 
+            'video_episode' => $video_episode, 
+            'subtitle' => $subtitle, 
+            'description' => $description, 
+            'genre' => $genre,
+            'collection' => $collection, 
+            'video_host' => $video_host, 
+            'video_id' => $video_id, 
+            'post_download_link' => $post_download_link, 
+            'download' => $download,
+            'year' => $post_year,
+            'video_rating' => $post_video_rating,
+            'video_thumbnail' => $video_thumbnail, 
+            'video_image_hover' => $video_image_hover,
+            'post_video_length' => $video_lenght,
+            'post_video_quality' => $video_quality,
+            'redes' => $redes,
+            'production' => $production,
+            'link' => $link
+        );
     }
 
 
