941e0a2e622eabbaffb32673858ad0d94e21bacf
mizael isaltino <mizael.isaltino@gmail.com>
2021-09-01 17:21:21 -0300

    -- Colocando segundo parâmetro de ordenação por data na rota /collection

--- app/wp-content/themes/feliz7play/classes/rotas_api/rest_collection.php
+++ app/wp-content/themes/feliz7play/classes/rotas_api/rest_collection.php
@@ -20,7 +20,7 @@ function get_rest_collection($data) {
       'post_type' => 'video',               
       'posts_per_page' => $per_page,               
       'paged' => $page,   
-      'orderby' => array($orderby, 'date' => 'ASC'),  
+      'orderby' => array($orderby => 'ASC', 'date' => 'ASC'),  
       'order' => $order,
       'post_status' => 'publish',
       'tax_query' => array(
