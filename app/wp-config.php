6772193839ee4be357af17846c33df8d91437a2f
webdsa <71404981+webdsa@users.noreply.github.com>
2021-09-03 12:52:20 -0300

    Update wp-config.php
    Altera o endereço da constante DOMAIN_CURRENT_SITE

--- app/wp-config.php
+++ app/wp-config.php
@@ -58,7 +58,7 @@ define('WP_ALLOW_MULTISITE', true);
 
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', false);
-define('DOMAIN_CURRENT_SITE', 'v2.feliz7play.com');
+define('DOMAIN_CURRENT_SITE', 'feliz7play.com');
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
