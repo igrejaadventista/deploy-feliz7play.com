14cc16990a8a5dc22e1d7403ec19d2e74302d8f0
Eli Mendonça <contato@elimendonca.com>
2021-09-09 09:17:43 -0300

    Instala NewRelic

--- app/setSeason.php
+++ app/setSeason.php
@@ -7,7 +7,9 @@
 function setSeasons() {
     $items = get_terms( 'collection' );	
     foreach ( $items as $item ) {
-        
+        update_field( 'seasons', false, "collection_". $item->term_id );
+        update_field( 'collection_enable', true, "collection_". $item->term_id );
+
         if ( count( get_term_children( $item->term_id, 'collection' ) ) > 0 ) {	
             $count++;
             echo $count ." - " . $item->term_id .", ". $item->name . "\n";
