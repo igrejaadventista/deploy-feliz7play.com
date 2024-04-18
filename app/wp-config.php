7d5eafdb76bcdb269ed1d96ed2a73ab1559c2b9b
DSA - Eli Mendonça <eli.mendonca@adventistas.org>
2024-04-18 07:00:31 -0300

    Ajuste nas chaves da AWS

--- app/wp-config.php
+++ app/wp-config.php
@@ -41,10 +41,8 @@ define( 'DISALLOW_FILE_EDIT', true );
 
 define( 'AS3CF_SETTINGS', serialize( array(
     'provider' => 'aws',
-    'access-key-id' => 'WP_S3_ACCESS_KEY',
-    // 
+    'access-key-id' => 'AWS_A_KEY',
     'secret-access-key' => 'AWS_S_KEY',
-    //
 	'bucket' => 'WP_S3_BUCKET'
 ) ) );
 
