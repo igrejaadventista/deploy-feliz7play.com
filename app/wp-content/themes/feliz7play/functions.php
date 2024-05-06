3596e50b10082770c2087644660f76dd7622c981
Leandro Nunes <lmnscout@gmail.com>
2024-05-06 17:19:20 -0300

    taxonomy radio buttons adjust

--- app/wp-content/themes/feliz7play/functions.php
+++ app/wp-content/themes/feliz7play/functions.php
@@ -205,7 +205,7 @@ function custom_taxonomy_radio_buttons() {
 				// Verifica se a categoria está selecionada e marca o radio button correspondente
 				$checked = '';
 				if ($(this).prop('checked')) {
-					$(this).closest('li').find('input[type="radio"]').prop('checked', 'checked');
+					// $(this).closest('li').find('input[type="radio"]').prop('checked', 'checked');
 					$checked = 'checked="checked"';
 				}
 				// Substitui o checkbox por um radio button
