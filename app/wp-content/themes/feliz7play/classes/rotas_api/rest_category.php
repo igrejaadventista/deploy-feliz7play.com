70f1e2876f35fd325544b371d148c9697a37f47b
Leandro Nunes <lmnscout@gmail.com>
2024-06-20 09:49:05 -0300

    add new field visible  on route /category 20

--- app/wp-content/themes/feliz7play/classes/rotas_api/rest_category.php
+++ app/wp-content/themes/feliz7play/classes/rotas_api/rest_category.php
@@ -1,11 +1,50 @@
 <?php
   
   add_action( 'rest_api_init', function(){
+	register_rest_route( 'wp/v3', '/category', array(
+	'methods' => 'GET',
+    'callback' => 'get_rest_geral_category'
+	));
+
 	register_rest_route( 'wp/v3', '/category/(?P<id>\d+)', array(
 	'methods' => 'GET',
     'callback' => 'get_rest_category'
 	));
 });
+
+function get_rest_geral_category($data) {
+
+    $page = $data->get_param('page');
+    $per_page = $data->get_param('per_page');
+    $id_exclude = $data->get_param('id_exclude');
+    $type_exclude = $data->get_param('type_exclude');
+
+    $args = array(
+        'post_type'         => 'video',
+        'posts_per_page'    => -1,
+        'post_status'       => 'publish',
+        'exclude'           => $id_exclude,
+        'tax_query' => array(
+            array(
+                'taxonomy' => 'category',
+                'field'    => 'category_visible',
+                'terms'    => true,
+            ),
+        ),
+    );
+    
+    $items = get_line_post_genre($args);
+
+
+       
+    $infos = pagination_array($items, $page, $per_page);
+    $final = $infos['paged'];
+    $resposta = new WP_REST_Response($final, 200);
+    $resposta->header('X-WP-Total', count($items));
+    $resposta->header('X-WP-TotalPages', $infos['totalPages']);
+
+    return $resposta;
+}
 
 function get_rest_category($data) {
 
