245b8521a0787383dac73804ae033039478481a3
Leandro Nunes <lmnscout@gmail.com>
2024-07-02 15:37:30 -0300

    add fields video_thumbnail on slider route 2

--- app/wp-content/themes/feliz7play/classes/rotas_api/rest_slider.php
+++ app/wp-content/themes/feliz7play/classes/rotas_api/rest_slider.php
@@ -207,7 +207,7 @@ function get_page_option($data)
 
 					$video_host =           get_field('post_video_host', $target);
 					$video_id =             get_field('post_video_id', $target);
-					$video_thumbnail =      get_field('video_thumbnail', $target);
+					$video_thumbnail =      get_field('video_thumbnail', $target)->url;
 					$extras = get_extras($target, 'video');
 
 				} else {
@@ -225,7 +225,7 @@ function get_page_option($data)
 					$collection_father = get_field('to_collection', $item->ID)->parent ? get_term(get_field('to_collection', $item->ID)->parent)->slug : false;
 					$video_host =  get_field('collection_video_host', 'term_' . $target);
 					$video_id =    get_field('collection_video_id', 'term_' . $target);
-					$video_thumbnail = get_field('collection_image', 'term_' . $target);
+					$video_thumbnail = get_field('collection_image', 'term_' . $target)->url;
 					$extras = get_extras($target, 'collection');
 				}
 
