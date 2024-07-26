e5c20105d52bd042d3c8b3d89a0955cdb823a5e5
Leandro Nunes <lmnscout@gmail.com>
2024-07-26 16:26:33 -0300

    fix video title on extras

--- app/wp-content/themes/feliz7play/classes/rotas_api/rest_slider.php
+++ app/wp-content/themes/feliz7play/classes/rotas_api/rest_slider.php
@@ -21,23 +21,23 @@ function get_extras($id, $field_name)
 					$video_id = $video_item['post_extra_video']->ID;
 					$meta = get_post_meta($video_id);
 			
-					$title =                $video_item['extra_video']->post_title;
-					$slug =                 $video_item['extra_video']->post_name;
-					$video_type =           $meta['post_video_type'][0];
-					$video_episode =        $meta['video_episode'][0];
-					$subtitle =             $meta['post_subtitle'][0];
-					$description =          wp_strip_all_tags($meta['post_blurb'][0]);
-					$video_host =           $meta['post_video_host'][0];
-					$video_id =             $meta['post_video_id'][0];
+					$title = $video_item->post_title;
+					$slug = $video_item->post_name;
+					$video_type = $meta['post_video_type'][0];
+					$video_episode = $meta['video_episode'][0];
+					$subtitle = $meta['post_subtitle'][0];
+					$description = wp_strip_all_tags($meta['post_blurb'][0]);
+					$video_host = $meta['post_video_host'][0];
+					$video_id = $meta['post_video_id'][0];
 			
-					$post_download_link =   $meta['link_download_app'][0];
-					$download =             $meta['download'][0];
-					$post_year =            $meta['post_year'][0];
-					$post_video_rating =    $meta['post_video_rating'][0];
-					$post_video_age_rating =    $meta['post_video_age_rating'][0];
-					$redes =                get_field('redes', $id);
-					$production =           get_field('production', $id);
-					$collection =           get_the_terms($id, 'collection')[0];
+					$post_download_link = $meta['link_download_app'][0];
+					$download = $meta['download'][0];
+					$post_year = $meta['post_year'][0];
+					$post_video_rating = $meta['post_video_rating'][0];
+					$post_video_age_rating = $meta['post_video_age_rating'][0];
+					$redes = get_field('redes', $id);
+					$production = get_field('production', $id);
+					$collection = get_the_terms($id, 'collection')[0];
 			
 					if ($collection) {
 						$collection->parent_slug = get_term($collection->parent, 'collection')->slug;
