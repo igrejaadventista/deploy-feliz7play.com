fff764dc1c1bad73cc44cebfd3f407cf8e42b4bd
Victor Guimarães <vguimaraes@studiovisual.com.br>
2024-10-22 14:51:56 -0300

    Adiciona retorno das linguagens nas chamadas de API do site-settings

--- app/wp-content/themes/feliz7play/classes/controllers/F7P_Site_Settings.class.php
+++ app/wp-content/themes/feliz7play/classes/controllers/F7P_Site_Settings.class.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 
 if( function_exists('acf_add_options_page') ) {
@@ -19,20 +19,15 @@ function rest_site_settings() {
     ) );
 }
 
-function get_site_settings(){
+function get_site_settings() {
+    $languages = get_field('languages', 'site_settings');
 
-    $variable['social_networks']['social_network'] = get_field('redes_sociais', 'site_settings');
-    $variable['social_networks']['meta_description'] = get_field('meta_description', 'site_settings');
-    $variable['social_networks']['meta_keywords'] = get_field('meta_keywords', 'site_settings');
-    $variable['social_networks']['share_image'] = get_field('share_image', 'site_settings');
-    $variable['menus']['header'] = get_field('header', 'site_settings');
-    $variable['menus']['footer'] = get_field('footer', 'site_settings');
-    $variable['menus']['sites'] = get_field('sites', 'site_settings');
-    $variable['options']['logo'] = get_field('logo_footer', 'site_settings');
-    $variable['options']['copyright'] = get_field('copyright', 'site_settings');
-    $variable['options']['imgErro404'] = get_field('imagem_erro_404', 'site_settings');
-    $variable['options']['link_feedback'] = get_field('link_feedback', 'site_settings');
-    $variable['options']['link_privacy_policy'] = get_field('link_privacy_policy', 'site_settings');
-    
-    return new WP_REST_Response( $variable , 200 );
+	if (!empty($languages)) {
+        $filtered_languages = [];
+		foreach ($languages as $language) {
+			$filtered_languages[$language['language']] = array_diff_key($language, ['language' => '']);
+		}
+	}
+
+    return new WP_REST_Response(isset($filtered_languages) && !empty($filtered_languages) ? $filtered_languages : $language, 200);
 }
