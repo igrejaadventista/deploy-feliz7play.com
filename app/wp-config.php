6911f28ee3da59499a4f0ed36286a313bbf41619
webdsa <71404981+webdsa@users.noreply.github.com>
2024-11-01 11:26:28 -0300

    Update wp-config.php

--- app/wp-config.php
+++ app/wp-config.php
@@ -41,12 +41,18 @@ define( 'DB_COLLATE', '' );
 
 define( 'DISALLOW_FILE_EDIT', true );
 
-define( 'AS3CF_SETTINGS', serialize( array(
-    'provider' => 'aws',
-    'access-key-id' => $_ENV['WP_S3_ACCESS_KEY'],
-    'secret-access-key' => $_ENV['WP_S3_SECRET_KEY'],
-	'bucket' => $_ENV['WP_S3_BUCKET']
-) ) );
+define('AS3CF_SETTINGS', serialize(array(
+	'provider' => 'aws',
+	'region' => 'us-east-1',
+	'access-key-id' => $_ENV['WP_S3_ACCESS_KEY'],
+	'secret-access-key' => $_ENV['WP_S3_SECRET_KEY'],
+	'bucket' => $_ENV['WP_S3_BUCKET'],
+	'enable-delivery-domain' => true,
+	'delivery-domain' => $_ENV['WP_S3_BUCKET'],
+	'signed-urls-object-prefix' => 'noticias.adventistas.org',
+    'force-https' => true,
+	'remove-local-file' => true,
+)));
 
 if(!$is_dev):
     define( 'FORCE_SSL', true );
