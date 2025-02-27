500c09955b7eb35980f5055d33709015ed837bcf
Victor Guimarães <vguimaraes@studiovisual.com.br>
2025-02-27 19:59:46 -0300

    Implementa função para tradução automática dos conteúdos

--- app/wp-content/themes/feliz7play/classes/controllers/F7P_Deepl.class.php
+++ app/wp-content/themes/feliz7play/classes/controllers/F7P_Deepl.class.php
@@ -2,22 +2,55 @@
 class Deepl {
 	public function __construct() {
 		add_action('init', function() {
-			$authKey = '337ca2f4-8e2b-41f2-ba1c-51382743985c:fx'; // Replace with your key
-			$deeplClient = new \DeepL\DeepLClient($authKey);
+			add_action('acf/save_post', function($post_id) {
+				if (get_post_type($post_id) !== 'video') {
+					return;
+				}
 
-			$result = $deeplClient->translateText('Hello, world!', null, 'fr');
-			echo $result->text;
+				$languages = get_field('languages', $post_id);
+				if (!$languages) {
+					return;
+				}
 
-			// add_action('acf/save_post', function($post_id) {
-			// 	if (get_post_type($post_id) !== 'video' || self::$auto_index !== 'on') {
-			// 		return;
-			// 	}
+				$default_language = '';
+				$current_post_languages = [];
+				foreach ($languages as $language_data) {
+					if ($language_data['language'] === 'pt') {
+						$default_language = $language_data;
+					}
+					$current_post_languages[] = $language_data['language'];
+				}
 
-			// 	$data_to_index = self::get_video_data($post_id);
-			// 	foreach ($data_to_index as $item) {
-			// 		self::index_data($item);
-			// 	}
-			// }, 10, 3);
+				$languages_to_translate = array_diff(['pt', 'es', 'en'], $current_post_languages);
+
+				if (empty($default_language) || empty($languages_to_translate)) {
+					return;
+				}
+
+				$authKey = '337ca2f4-8e2b-41f2-ba1c-51382743985c:fx';
+				$deeplClient = new \DeepL\DeepLClient($authKey);
+
+				foreach ($languages_to_translate as $language_to_translate) {
+					$string_to_translate = $default_language['title'] . ' : ' . $default_language['post_subtitle'] . ' : ' . $default_language['slug'] . ' : ' . $default_language['post_blurb'];
+
+					$translation = $deeplClient->translateText(
+						$string_to_translate,
+						null,
+						$language_to_translate === 'en' ? 'en-us' : $language_to_translate
+					);
+
+					$translation = explode(': ', $translation->text);
+
+					$row = $default_language;
+					$row['language'] = $language_to_translate;
+					$row['title'] = $translation[0];
+					$row['post_subtitle'] = $translation[1];
+					$row['slug'] = $translation[2];
+					$row['post_blurb'] = $translation[3];
+
+					add_row('field_670ff24637fba', $row, $post_id);
+				}
+			}, 10, 3);
         }, 99);
 	}
 }
